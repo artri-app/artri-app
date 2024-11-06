45837b54728dce71db1a3786a003e7a54ce12b04
andreendo <andreendo@gmail.com>
2024-11-06 16:50:32 -0300

    initial structure

---
+++ lib/ui/app.dart
@@ -0,0 +1,18 @@
+import 'package:artriapp/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ArtriApp',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'ArtriApp'),
+    );
+  }
+}
