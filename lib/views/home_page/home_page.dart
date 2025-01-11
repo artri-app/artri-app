e654f5407521348df31588bbcfff76c94b5c7106
matheusymm <matheusyuitimoriy@gmail.com>
2025-01-11 12:33:36 -0300

    feat: add config and change email views

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -1,5 +1,7 @@
-import 'package:artriapp/views/widgets/navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:artriapp/views/widgets/input_text.dart';
+import 'package:artriapp/views/widgets/navbar.dart';
+import 'package:artriapp/views/config_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -19,12 +21,26 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _navigateToConfigPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ConfigPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Settings',
+            onPressed: () => _navigateToConfigPage(context),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -37,6 +53,7 @@ class _HomePageState extends State<HomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const InputText(placeholder: 'Placeholder'),
           ],
         ),
       ),
