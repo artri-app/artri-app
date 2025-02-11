5a601c7337489656080d2df1eca2abd226abf77c
Vitor Recoaro <vitordearec@gmail.com>
2025-02-10 22:16:38 -0300

    feat: auth service
    Auth service to call API to handle authentication requests, as login, register, password change and others.

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:artriapp/models/generic_api_response.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  final String apiUrl = 'http://18.222.85.109:8000/api';
+
+  Future<GenericApiResponse> login(String user, String password) async {
+    final response = await http.post(Uri.parse("$apiUrl/login"),
+        body: {"username": user, "password": password});
+
+    return GenericApiResponse.fromJson(jsonDecode(response.body));
+  }
+}
