c8e3feebf957d7f222824e8f8f6e5eab854b52df
matheusymm <matheusyuitimoriy@gmail.com>
2025-03-05 09:31:28 -0300

    add specific info pages layout

---
+++ lib/views/info/data/minha_dor.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MinhaDorInfoPage extends StatelessWidget {
+  const MinhaDorInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff025940),
+        title: const Text(
+          'Minha Dor',
+        ),
+        centerTitle: true,
+        titleTextStyle: GoogleFonts.montserrat(
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 24,
+          ),
+        ),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 48,
+            height: 48,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2.0,
+              ),
+            ),
+            child: const Center(
+              child: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+                size: 24,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Título',
+              style: GoogleFonts.montserrat(
+                textStyle: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Gap(10),
+            Text(
+              'Descrição 1',
+              style: GoogleFonts.montserrat(
+                textStyle: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const Gap(20),
+            Text(
+              'Subtítulo 1',
+              style: GoogleFonts.montserrat(
+                textStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const Gap(10),
+            Text(
+              'Descrição 2',
+              style: GoogleFonts.montserrat(
+                textStyle: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const Gap(20),
+            Text(
+              'Subtítulo 2',
+              style: GoogleFonts.montserrat(
+                textStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const Gap(10),
+            Text(
+              'Descrição 3',
+              style: GoogleFonts.montserrat(
+                textStyle: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
