f6bce8a596bedd132f5bb520542a3bb36a0276f0
Vitor Recoaro <vitordearec@gmail.com>
2025-08-24 19:41:41 -0300

    feat: auth service
    Adding the auth service for login and registration.

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 
-import 'package:artriapp/models/generic_api_response.dart';
+import 'package:artriapp/models/api_responses/generic_api_response.dart';
+import 'package:artriapp/utils/env_variables.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  final String _apiUrl = 'http://18.222.85.109:8000/api';
+  final String _apiUrl = Environment.apiUrl;
 
   Future<GenericApiResponse> login(String user, String password) async {
-    final response = await http.post(Uri.parse("$_apiUrl/login"),
+    final response = await http.post(Uri.parse("$_apiUrl/login/"),
         body: {"username": user, "password": password});
 
     return GenericApiResponse.fromJson(jsonDecode(response.body));
