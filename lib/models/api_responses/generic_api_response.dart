29cdbf8823b9320022e2bba9ac41e22a60c51d29
Vitor Recoaro <vitordearec@gmail.com>
2025-09-03 22:13:47 -0300

    feat: auth service
    Adding the current endpoints to user authentication.

--- lib/models/api_responses/generic_api_response.dart
+++ lib/models/api_responses/generic_api_response.dart
@@ -1,14 +1,19 @@
 class GenericApiResponse {
-  final String message;
+  final String? message;
+  final bool isSuccess;
 
-  const GenericApiResponse({required this.message});
+  const GenericApiResponse({required this.message, required this.isSuccess});
 
   factory GenericApiResponse.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
         'message': String msg,
       } =>
-        GenericApiResponse(message: msg),
+        GenericApiResponse(message: msg, isSuccess: true),
+      {
+        'error': String error,
+      } =>
+        GenericApiResponse(message: error, isSuccess: false),
       _ => throw const FormatException(
           'Invalid structure of Generic Api Response'),
     };
