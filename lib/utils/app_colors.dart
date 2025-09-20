5baeffacca94e2c6673cdfe7849f76d4f713d060
Vitor Recoaro <vitordearec@gmail.com>
2025-09-20 10:47:58 -0300

    feat: dark blue color
    Adding new color on app colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class AppColors {
   static const Color darkGreen = Color(0xff025940);
   static const Color lightGreen = Color(0xff6EFF00);
+  static const Color darkBlue = Color(0xff026873);
 }
